a45dcda2f5aca45b8adec676a0b0ca94faea8979
zafarbek <zafarmuxiddinov8@gmail.com>
2023-05-12 21:57:37 +0500

    Portfolio

--- product/models.py
+++ product/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     parent = models.ForeignKey('Category', verbose_name="Kategoriyasi",
                                null=True, blank=True, on_delete=models.PROTECT)
     name = models.CharField(verbose_name="Kategoriya nomi", max_length=255, null=False, blank=False)
+
     def __str__(self):
         return self.name
 
@@ -15,5 +16,7 @@ class Product(models.Model):
     full_name = models.CharField("Maxsulotning to'liq nomi", max_length=255)
     price = models.DecimalField(verbose_name="Maxsulot narxi", max_digits=17, decimal_places=2)
     describtion = models.TextField(verbose_name="Maxsulot haqida ma'lumot")
+
+
     def __str__(self):
         return self.name
--- product/serializers.py
+++ product/serializers.py
@@ -2,11 +2,28 @@ from rest_framework.serializers import ModelSerializer
 from .models import Category, Product
 
 
-class CategorySerializer(ModelSerializer):
+class ChildCategorySerializer(ModelSerializer):
 
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ("id", "name",)
+
+
+class ChildProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class CategorySerializer(ModelSerializer):
+    child_category_obj = ChildCategorySerializer(source='child_product',
+                                                 many=True, read_only=True)
+    product_obj = ChildProductSerializer(source='product_relate',
+                                         many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = "id", "name", "parent", "child_category_obj", "product_obj",
 
 
 class ProductSerializer(ModelSerializer):
